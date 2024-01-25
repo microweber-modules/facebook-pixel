8bd3137baac608fb762a1e7f636a0f0d5aaea998
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-25 16:47:06 +0200

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,15 +7,5 @@ Route::name('facebook-pixel.')
     ->group(function () {
 
         Route::get('/export-feed', 'FacebookPixelExportController@index')->name('export-feed');
-
-        Route::get('/faawf', function () {
-
-           // event($event = new \MicroweberPackages\Order\Events\OrderWasCreated(\MicroweberPackages\Order\Models\Order::find(9), []));
-
-            event(new \MicroweberPackages\SiteStats\Events\PingStatsEvent([
-                'referer'=>site_url(),
-            ]));
-
-        });
-
+        
     });
