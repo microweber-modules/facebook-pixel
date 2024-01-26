76f112d704539e787ca94395239cd95c0b2755e6
Bozhidar Slaveykov <bobi@microweber.com>
2024-01-26 17:56:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $config['ui_admin_iframe'] = true;
 $config['ui'] = true;
 $config['categories'] = "content";
 $config['position'] = 200;
-$config['version'] = 1.3;
+$config['version'] = 1.4;
 $config['settings'] = [];
 
 // Here is the index route for admin panel
